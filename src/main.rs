b844ef83ec4006342b86c2943e2b63ff7edb2013
muitsfriday <mu.itsfriday@gmail.com>
2022-01-07 02:58:06 +0700

    add a call from main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use module_example::Command;
+use module_example::Controller;
+
 fn main() {
-    println!("Hello, world!");
+    let ctrl = Controller::new("Hello");
+    ctrl.describe();
+    let r = Controller::compute(Command::Mul(10, 32));
+
+    println!("Hello, world! {:?}", r);
 }
